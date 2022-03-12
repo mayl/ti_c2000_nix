08e69c3aa27ea053aa6617042c4c452128b654e1
Larry May <larrymayster@gmail.com>
2022-03-12 13:24:50 -0500

    extract out file name

--- default.nix
+++ default.nix
@@ -1,26 +1,29 @@
-{patchelf, stdenv, glib, fetchurl, lib, ...}:
+{patchelf, stdenv, fetchurl, lib, ...}:
 stdenv.mkDerivation rec{
   pname = "ti_c2000";
-  version = "21.12.0.STS";
+  version = "20.12.0.STS";
+
+  installFile = "ti_cgt_c2000_${version}_linux-x64_installer.bin";
 
   src = fetchurl {
-    url = "https://dr-download.ti.com/software-development/ide-configuration-compiler-or-debugger/MD-xqxJ05PLfM/20.12.0.STS/ti_cgt_c2000_20.12.0.STS_linux-x64_installer.bin";
+    url = "https://dr-download.ti.com/software-development/ide-configuration-compiler-or-debugger/MD-xqxJ05PLfM/20.12.0.STS/${installFile}";
     sha256 = "CaXDCJVmMKYcTaJ3yCCUcuoAk+zRAmFJ8Jiy2q7xn5E=";
   };
-
-  nativeBuildInputs = [
-    glib
-  ];
 
   sourceRoot = ".";
 
   dontUnpack = true;
 
+  unpackPhase = ''
+    chmod u+rwx $src
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/
+  '';
+
   installPhase = '' 
     mkdir -p $out
-    cp $src $out/ti_cgt_c2000_20.12.20.STS_linux-x64_installer.bin
-    chmod u+rwx $out/ti_cgt_c2000_20.12.20.STS_linux-x64_installer.bin
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/ti_cgt_c2000_20.12.20.STS_linux-x64_installer.bin
-    $out/ti_cgt_c2000_20.12.20.STS_linux-x64_installer.bin --mode unattended --prefix $out
+    cp $src $out/$installFile
+    chmod u+rwx $out/$installFile
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/$installFile
+    $out/$installFile --mode unattended --prefix $out
   '';
 }
